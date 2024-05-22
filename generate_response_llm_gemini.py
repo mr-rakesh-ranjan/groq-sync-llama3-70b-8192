a15f0f9cc7d1c7e8c13214b80713fd8b3ff0e652
Msahana36 <sahana_m@pursuitsoftware.biz>
2024-05-22 21:30:07 +0530

    edited enhance-coverage url

--- generate_response_llm_gemini.py
+++ generate_response_llm_gemini.py
@@ -42,7 +42,7 @@ def createDynamicActions(policyNumber, premiumBalance, accountNumber):
 		},
 		{ 
 			'label':'Show Coverage',
-			'url': f'/api/v1/llm/{policyNumber}/{accountNumber}/coverage-details'
+			'url': f'/api/v1/sql/{policyNumber}/{accountNumber}/coverage-details'
 		}
     ]
     return actions
